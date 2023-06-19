00518a11c576acad67f2793511c0f943df1a94ad
amirb <amir.bullock9@gmail.com>
2023-06-19 13:51:37 -0400

    stub up function to rate abyss

--- controllers/abysses.js
+++ controllers/abysses.js
@@ -14,6 +14,30 @@ function index(req, res) {
 		})
 }
 
+function rateAbyss(req, res) {
+  const { abyssId } = req.params;
+  const { rating } = req.body;
+
+  Abyss.findById(abyssId)
+    .then((abyss) => {
+      if (!abyss) {
+        return res.status(404).send('Abyss not found');
+      }
+
+      // Save the rating to the abyss
+      abyss.ratings.push(rating);
+      return abyss.save();
+    })
+    .then(() => {
+      res.redirect('/abyss-all');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/abyss-all');
+    });
+}
+
 export {
   index,
+	rateAbyss,
 }
--- routes/abysses.js
+++ routes/abysses.js
@@ -3,7 +3,7 @@ import * as abyssesCtrl from '../controllers/abysses.js'
 const router = Router()
 
 router.get('/abyss-all', abyssesCtrl.index)
-
+router.post('/rate-abyss/:abyssId', abyssesCtrl.rateAbyss)
 export {
   router
 }
