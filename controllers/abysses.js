15e07ccb948051a072b70888d15813a7096ed342
amirb <amir.bullock9@gmail.com>
2023-07-01 21:05:51 -0400

    stub up add enemy functionality within abyss

--- controllers/abysses.js
+++ controllers/abysses.js
@@ -38,49 +38,58 @@ function rateAbyss(req, res) {
 }
 
 function newAbyss(req, res) {
-	const { user } = req
+  const { user } = req;
 
-	if (user && user.admin) {
-
-		Abyss.find({})
-		.then(abysses => {
-			res.render('abysses/new-abyss', {
-				abysses,
-				title: "Abyss Creator",
-			})
-		})
-		.catch(err => {
-			console.log(err)
-			res.redirect('abysses/abyss-all')
-		})
-	} else {
-		res.status(403).send("Unauthorized access")
-	}
+  if (user && user.admin) {
+    Promise.all([Abyss.find({}), Enemy.find({})])
+      .then(([abysses, enemies]) => {
+        res.render('abysses/new-abyss', {
+          abysses,
+          enemies,
+          title: "Abyss Creator",
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.redirect('/abysses/abyss-all');
+      });
+  } else {
+    res.status(403).send("Unauthorized access");
+  }
 }
 
 function createAbyss(req, res) {
   const { user } = req
 
   if (user && user.admin) {
-    const { title, startDate, endDate, content } = req.body
+    const { title, startDate, endDate, content, enemies, levels } = req.body
 
     const newAbyss = new Abyss({
       title,
       startDate,
       endDate,
       content,
+      enemies: [] // Initialize the enemies array
     })
 
+    // Loop through the enemies and levels arrays to create an enemy object with its corresponding level
+    for (let i = 0; i < enemies.length; i++) {
+    const enemy = {
+      enemyId: enemies[i],
+      level: levels[i]
+    }
+    newAbyss.enemies.push(enemy)
+    }
+    console.log(newAbyss)
+    console.log(req.body)
     newAbyss.save()
-      .then(() => {
-        res.redirect('abyss-all')
-      })
-      .catch((err) => {
-        console.log(err)
-        res.redirect('new-abyss')
-      })
-  } else {
-    res.status(403).send("Unauthorized access")
+    .then(() => {
+      res.redirect('/abysses/abyss-all')
+    })
+    .catch((err) => {
+      console.log(err)
+      res.redirect('/abysses/new-abyss')
+    })
   }
 }
 
